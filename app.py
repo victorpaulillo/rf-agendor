862327fb60e821571cacefdc6c4351f652bee062
Victor Paulillo <victor.paulillo@gmail.com>
2021-09-03 14:43:57 -0300

    Adicionado o pubsub como trigger e para iniciar o prox job

--- app.py
+++ app.py
@@ -3,15 +3,28 @@ import base64
 import requests
 from datetime import datetime
 from google.cloud import storage
+from google.cloud import pubsub_v1
+from concurrent import futures
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def download():
+def download_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
     start_time = datetime.now()
+
     # pubsub_message = 'F.K03200$Z.D10710.MOTICSV.zip'
-    pubsub_message = 'K3241.K03200Y0.D10710.ESTABELE.zip'
+    # pubsub_message = 'K3241.K03200Y0.D10710.ESTABELE.zip'
+
     url = 'http://200.152.38.155/CNPJ/' + pubsub_message 
     myfile = requests.get(url)
     down_time = datetime.now()
@@ -21,9 +34,7 @@ def download():
 
     print('Down time: ', down_time - start_time)
     print('Download time: ', download_time - start_time)
-    print('terminei de baixar')
-
-
+    print('Finished downloading')
 
     bucket_name = "cnpj-rf"
     file_name = pubsub_message
@@ -38,10 +49,30 @@ def download():
     upload_time = datetime.now()
 
     print('Upload time: ', upload_time-start_time)
-
     print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
 
+    """Publishes messages to a Pub/Sub topic"""
+
+    # TODO(developer)
+    project_id = "cnpj-rf-324200"
+    topic_id = "downloaded_files"
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    data = str(pubsub_message)
+    # Data must be a bytestring
+    data = data.encode("utf-8")
+    # Add two attributes, origin and username, to the message
+    future = publisher.publish(
+        topic_path, data, origin="python-sample", username="gcp"
+    )
+    print(future.result())
+
+    print(f"Published messages with custom attributes to {topic_path}.")
+
     return "Hello {}!".format(pubsub_message)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
