ac69fc9f7119d828ed28c47a524edcd20bc0b93f
Victor Paulillo <victor.paulillo@gmail.com>
2021-11-08 12:12:30 -0300

    Teste loop operador airflow v23

--- dag_rf_cnpj.py
+++ dag_rf_cnpj.py
@@ -34,6 +34,41 @@ dag = DAG(
 )
 
 
+def remove_spec_char(**kwargs):
+    ti = kwargs['ti']
+    file_number = kwargs.get('file_number')
+    bucket_name = kwargs.get('bucket_name')
+    file_name_ = ti.xcom_pull(task_ids='list_files_rf')[int(file_number)]
+
+    if file_name_ == '':
+        None
+
+    else:
+        if 'ESTABELE' in file_name_:
+
+            file_name_final = file_name_[:-4]
+            blob_name = "unzip_files/" + file_name_final
+            new_blob_special_character = "unzip_files_treated/" + file_name_final
+
+            client = storage.Client()
+            bucket = client.get_bucket(bucket_name)
+
+            blob = bucket.get_blob(blob_name)
+            file = blob.download_as_string()
+
+            file_decoded = file.decode("ISO-8859-1")
+            file_upload = re.sub(r"[^a-zA-Z0-9,-@+_ \"\n]", '', str(file_decoded))
+            
+            bucket.blob(new_blob_special_character).upload_from_string(file_upload, 'text/csv')
+            
+            print(f'File moved from {blob_name} to {new_blob_special_character}')
+
+        else:
+            print(f'Did nothing, file is not ESTABELE, it is {file_name_}')
+
+
+
+
 
 start_dag = DummyOperator(task_id='start_dag', dag=dag)
 
@@ -46,11 +81,14 @@ def values_function():
 
 def group_remove_speacial_char(number, **kwargs):
         #load the values if needed in the command you plan to execute
-        dyn_value = "{{ task_instance.xcom_pull(task_ids='push_func') }}"
-        return BashOperator(
-                task_id='JOB_NAME_{}'.format(number),
-                bash_command='script.sh {} {}'.format(dyn_value, number),
-                dag=dag)
+        file_number = "{{ task_instance.xcom_pull(task_ids='push_func') }}"
+        return PythonOperator(task_id='remove_spec_char_{}'.format(file_number),
+            dag=dag,python_callable=remove_spec_char,
+            op_kwargs={"bucket_name":'cnpj_rf', "file_number":file_number}
+            )
+        
+        
+
                 
 
 push_func = PythonOperator(
@@ -59,10 +97,7 @@ push_func = PythonOperator(
         python_callable=values_function,
         dag=dag)
 
-complete = DummyOperator(
-        task_id='All_jobs_completed',
-        dag=dag)
 
 for i in values_function():
-        push_func >> group_remove_speacial_char(i) >> complete
+        push_func >> group_remove_speacial_char(i) 
 
