3df6517a6ed60774ed12e5f6401ad8ae8f6944d6
Victor Paulillo <victor.paulillo@gmail.com>
2021-11-26 19:11:36 -0300

    teste

--- dag_rf_cnpj.py
+++ dag_rf_cnpj.py
@@ -425,7 +425,7 @@ def insert_tmp_table_postgres():
                 correio_eletronico,
                 porte,
                 razao_social,
-                cast(capital_social as float) as capital_social,
+                capital_social::double precision as capital_social,
                 natureza_juridica ,
                 cnpj_basico ,
                 socios_json 
