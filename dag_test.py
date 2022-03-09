35b2fedea94972be5a05545c6b89f31a166a8506
Victor Paulillo <victor.paulillo@gmail.com>
2022-03-09 17:43:46 -0300

    Test Secret Manager

--- dag_test.py
+++ dag_test.py
@@ -53,4 +53,41 @@ bigquery_to_storage = PythonOperator(
     )
 
 
+def access_secret_version(project_id, secret_id, version_id):
+    """
+    Access the payload for the given secret version if one exists. The version
+    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+    """
+
+    # Import the Secret Manager client library.
+    from google.cloud import secretmanager
+
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+
+    # Print the secret payload.
+    #
+    # WARNING: Do not print the secret in a production environment - this
+    # snippet is showing how to access the secret material.
+    payload = response.payload.data.decode("UTF-8")
+    print("Plaintext: {}".format(payload))
+
+
+
+test_secret_manager = PythonOperator(
+    task_id='db_pass',
+    dag=dag,
+    python_callable=db_pass,
+    op_kwargs={"project_id":'rf-agendor-335020', "secret_id":'test', "version_id":'latest'}
+    )
+
+
+
+
     
